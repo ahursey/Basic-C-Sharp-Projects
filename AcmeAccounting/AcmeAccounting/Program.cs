09b6dfff3f3c7cf0f8fb5edc514c747a26c79763
Aaron Hursey <hursey.aaron@gmail.com>
2018-08-25 14:53:23 -0700

    ACME Accounting Console

---
+++ AcmeAccounting/AcmeAccounting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AcmeAccounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
