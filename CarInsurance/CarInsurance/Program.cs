0f513b1f1c48e9c91d5ba6ead1e1f80e2a0b8536
Aaron Hursey <hursey.aaron@gmail.com>
2018-08-26 14:40:27 -0700

    Added car insurance program

---
+++ CarInsurance/CarInsurance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt16(Console.ReadLine()) ;
+
+            Console.WriteLine("Have you ever had a DUI? Please answer \"True\" or \"False\".");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt16(Console.ReadLine());
+
+            bool qualified = age > 15 && DUI == false && tickets < 3;
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(qualified);
+
+            Console.ReadLine();
+
+        }
+    }
+}
