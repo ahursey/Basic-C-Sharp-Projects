1ae7bb34d073cc4b4645aa71aaf5c11ba2f1b1df
Aaron Hursey <hursey.aaron@gmail.com>
2018-08-26 15:05:51 -0700

    Added Package Shipping App

---
+++ ShippingQuoteApp/ShippingQuoteApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingQuoteApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.WriteLine("Please enter the package weight.");
+            decimal pkgWeight = Convert.ToDecimal(Console.ReadLine());
+
+            if (pkgWeight > 50) 
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+            }
+
+            else
+            {
+                Console.WriteLine("Please enter the package width:");
+                decimal pkgWidth = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package height:");
+                decimal pkgHeight = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("Please enter the package length:");
+                decimal pkgLength = Convert.ToDecimal(Console.ReadLine());
+
+                decimal pkgDimensions = pkgWidth + pkgHeight + pkgLength;
+
+                if (pkgDimensions > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                    Console.ReadLine();
+                }
+
+                else
+                {
+                    decimal total = (pkgDimensions * pkgWeight) / 100;
+                    Console.WriteLine("Your estimated total for shipping is: $" + total);
+                    Console.WriteLine("Thank you.");
+                    Console.ReadLine();
+                }
+
+            }
+        }
+    }
+}
