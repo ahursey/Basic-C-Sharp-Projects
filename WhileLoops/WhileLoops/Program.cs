785ac810d8a6dddbdaf4d36819487013c6d89a0d
Aaron Hursey <hursey.aaron@gmail.com>
2018-08-29 14:32:48 -0700

    Added a "do-while" program

---
+++ WhileLoops/WhileLoops/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is the answer to life, the universe, and everything?");
+            string answer = Console.ReadLine();
+            bool guess = answer == "42";
+
+            do
+            {
+                switch(answer)
+                {
+
+                    case "42":
+                        Console.WriteLine("Correct.");
+                        guess = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Incorrect. Try again.");
+                        answer = Console.ReadLine();
+                        break;
+
+                }
+            }
+
+            while (!guess);
+            Console.ReadLine();
+
+        }
+    }
+}
