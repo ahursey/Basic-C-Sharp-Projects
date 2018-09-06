a03cb04b06a085614af85cf6d6b4172e99a86ceb
Aaron Hursey <hursey.aaron@gmail.com>
2018-09-06 16:35:28 -0700

    Added first method/class tutorial and program

---
+++ MethodClass1/MethodClass1/MathOp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass1
+{
+    public class MathOp
+    {
+        public MathOp(int x)
+        {
+            this.x = x * 5;
+        }
+        public int x { get; set; }
+    }
+
+    public class MathDivide
+    {
+        public MathDivide(int y)
+        {
+            this.y = y / 5;
+        }
+        public int y { get; set; }
+    }
+
+    public class MathSquare
+    {
+        public MathSquare(int z)
+        {
+            this.z = z * z;
+        }
+        public int z { get; set; }
+    }
+}
---
+++ MethodClass1/MethodClass1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter an integer value.");
+            int value = Convert.ToInt32(Console.ReadLine());
+
+            MathOp result = new MathOp(value);
+            MathDivide result2 = new MathDivide(value);
+            MathSquare result3 = new MathSquare(value);
+          
+            Console.WriteLine(result.x);
+            Console.WriteLine(result2.y);
+            Console.WriteLine(result3.z);
+            Console.ReadLine();
+   
+        }
+    }
+}
