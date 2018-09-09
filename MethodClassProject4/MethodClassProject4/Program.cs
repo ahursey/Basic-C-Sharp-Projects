08b8b71650caa186479f9d73b9b32041ab1934c5
Aaron Hursey <hursey.aaron@gmail.com>
2018-09-09 14:47:32 -0700

    Added method/class project 4

---
+++ MethodClassProject4/MethodClassProject4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassProject4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            optionalClass result = new optionalClass();
+
+            Console.WriteLine("Please enter an integer:");
+            int firstEntry = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please optionally enter a second integer:");
+            string input = Console.ReadLine();
+            int secondEntry = 0;
+            if (!string.IsNullOrEmpty(input))
+            {
+                secondEntry = Convert.ToInt16(input);
+                result.optionalModifier(firstEntry, secondEntry);
+                Console.WriteLine("The first modified entry is: " + result.x + " and the second modified entry is: " + result.y);
+
+            }
+            else
+            {
+                result.optionalModifier(firstEntry);
+                Console.WriteLine("The first modified entry is: " + result.x);
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MethodClassProject4/MethodClassProject4/optionalClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassProject4
+{
+    public class optionalClass
+    {
+
+        public void optionalModifier(int x, int y = 0)
+        {
+            this.x = x * 10;
+            this.y = y + 10;
+        }
+
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+}
