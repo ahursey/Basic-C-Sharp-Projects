8d36c474d6dafe76d80a434831c3a947d6510008
Aaron Hursey <hursey.aaron@gmail.com>
2018-09-09 14:14:23 -0700

    Added method/class project 3

---
+++ MethodClassProject3/MethodClassProject3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassProject3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter a value to be operated on.");
+            int entry = Convert.ToInt32(Console.ReadLine());
+
+            integerClass result = new integerClass();
+            result.integerMod(entry);
+            Console.WriteLine("The result is: " + result.x);
+            Console.ReadLine();
+
+            decimal entry2 = Convert.ToDecimal(entry);
+            result.integerMod(entry2);
+            Console.WriteLine("The result is: " + result.y);
+            Console.ReadLine();
+
+            string entry3 = Convert.ToString(entry);
+            result.integerMod(entry3);
+            Console.WriteLine("The result is: " + result.w);
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ MethodClassProject3/MethodClassProject3/integerClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassProject3
+{
+    public class integerClass
+    {
+
+        public void integerMod(int x)
+        {
+            this.x = x + 12;
+        }
+
+        public void integerMod(decimal y)
+        {
+            this.y = y / 10;
+        }
+
+        public void integerMod(string z)
+        {
+            int w = Int32.Parse(z);
+            this.w = w * 5;
+        }
+
+        public int x { get; set; }
+        public decimal y { get; set; }
+        public string z { get; set; }
+        public int w { get; set; }
+
+    }
+}
