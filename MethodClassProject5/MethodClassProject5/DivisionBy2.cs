520bf3e754653961cb7890b08081a84b3f3371e8
Aaron Hursey <hursey.aaron@gmail.com>
2018-09-10 11:37:41 -0700

    Added method/class project 5

---
+++ MethodClassProject5/MethodClassProject5/DivisionBy2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassProject5
+{
+    public class DivisionBy2
+    {
+        public void Division(int x, out int y)
+        {
+            y = x / 2;
+        }
+
+        public static void Division(decimal w, out decimal z)
+        {
+            z = w / 2;
+        }
+
+        int x { get; set; }
+        int y { get; set; }
+        decimal w { get; set; }
+        decimal z { get; set; }
+
+    }
+}
---
+++ MethodClassProject5/MethodClassProject5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassProject5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter an integer that will be divied by 2:");
+            int entry = Convert.ToInt32(Console.ReadLine());
+            int answer = 0;
+
+            DivisionBy2 adjusted = new DivisionBy2();
+            adjusted.Division(entry, out answer);
+            Console.WriteLine("Your entered value divided by 2 is: " + answer);
+
+            Console.WriteLine("Please enter another integer that will be divied by 2:");
+            decimal entry2 = Convert.ToInt32(Console.ReadLine());
+            decimal answer2 = 0;
+
+            DivisionBy2.Division(entry2, out answer2);
+            Console.WriteLine(answer2);
+            Console.ReadLine();
+            
+        }
+    }
+}
