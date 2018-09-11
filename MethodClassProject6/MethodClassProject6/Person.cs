7072161da9401a242a36f04476b6e487bb490e20
Aaron Hursey <hursey.aaron@gmail.com>
2018-09-11 10:13:54 -0700

    Added class/method project 6

---
+++ MethodClassProject6/MethodClassProject6/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassProject6
+{
+    public class Person
+    {
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName + ".");
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ MethodClassProject6/MethodClassProject6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassProject6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee numberOne = new Employee();
+            numberOne.FirstName = "Sample";
+            numberOne.LastName = "Student";
+            numberOne.SayName();
+            Console.ReadLine();
+
+        }
+    }
+}
