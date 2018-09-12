c6a9a5d152e79fedf52edd6c13da013d5b73a7ec
Aaron Hursey <hursey.aaron@gmail.com>
2018-09-12 10:00:11 -0700

    Added interface project

---
+++ InterfaceProject/InterfaceProject/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceProject
+{
+    class Employee : IQuittable
+    {
+        void IQuittable.Quit()
+        {
+            Console.WriteLine("You have quit the game.");
+        }
+    }
+}
