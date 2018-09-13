32684ca74372b1b7f6bec687626fa4fd7615c532
Aaron Hursey <hursey.aaron@gmail.com>
2018-09-13 14:40:14 -0700

    Added structure exercise

---
+++ StructureExercise/StructureExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructureExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Number value = new Number();
+            Console.WriteLine("Please enter a number value:");
+            value.Amount = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(value.Amount);
+            Console.ReadLine();
+
+        }
+    }
+}
