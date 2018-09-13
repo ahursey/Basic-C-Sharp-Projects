919cb00431f2fbef9d062f0c00c0662bc5c2ca6f
Aaron Hursey <hursey.aaron@gmail.com>
2018-09-13 11:35:02 -0700

    Added generic list exercise

---
+++ Generics/Generics/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Employee<T>
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int employeeID { get; set; }
+        public List<T> things {get; set;}
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee<string> empList = new Employee<string>();
+            Employee<int> empInt = new Employee<int>();
+            empList.things = new List<string>();
+            empList.things.Add("Albert");
+            empList.things.Add("Montgomery");
+            empInt.things = new List<int>();
+            empInt.things.Add(20);
+            empInt.things.Add(30);
+
+            foreach (string item in empList.things)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (int item in empInt.things)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
