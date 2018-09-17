ddb1551a7afd2ad0e5d6646516b89f33feb46625
Aaron Hursey <hursey.aaron@gmail.com>
2018-09-17 15:46:16 -0700

    Added file writing exercise

---
+++ FileWriting/FileWriting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileWriting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter some text to be saved to a text file.");
+
+            string text = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Student\Log.txt", text);
+
+            Console.ReadLine();
+        }
+    }
+}
