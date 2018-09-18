8c784a5cf7c0346e01391e2501b3ddc2972cfe1c
Aaron Hursey <hursey.aaron@gmail.com>
2018-09-17 22:31:08 -0700

    Added additional feature exercise

---
+++ AdditionalFeaturesExercise/AdditionalFeaturesExercise/DaisyChain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeaturesExercise
+{
+    public class DaisyChain
+    {
+        public DaisyChain(int ID, Guid uniqueID, string userName)
+        {
+            uniqueID = Guid.NewGuid();
+        }
+
+        public DaisyChain(int ID, string userName) : this(ID, Guid.NewGuid(), userName)
+        {
+            uniqueID = Guid.NewGuid();
+        }
+
+    public int ID { get; set; }
+    public Guid uniqueID { get; set; }
+    public string userName { get; set; }
+}
+}
---
+++ AdditionalFeaturesExercise/AdditionalFeaturesExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeaturesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string impermenance = "Some things never change....";
+            Console.WriteLine(impermenance);
+            Console.ReadLine();
+
+            var trial = new DaisyChain(22, "testUser");
+            Console.WriteLine(trial.uniqueID);
+            Console.ReadLine();
+
+
+        }
+    }
+}
