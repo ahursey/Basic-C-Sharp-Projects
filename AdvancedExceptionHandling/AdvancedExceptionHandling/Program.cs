2dcfc64fa8be48c2e54b46330f2b522cefc92ded
Aaron Hursey <hursey.aaron@gmail.com>
2018-09-18 11:14:52 -0700

    Added advanced exception handling exercise

---
+++ AdvancedExceptionHandling/AdvancedExceptionHandling/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please enter your age in years.");
+            retry:
+            int age = 0;
+            try
+            {
+                age = Convert.ToInt16(Console.ReadLine());
+                if (age <= 0)
+                {
+                    Console.WriteLine("A negative number or zero was entered. Please enter only positive integer values.");
+                    goto retry;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The wrong datatype was entered. Please only enter positive integer values.");
+                goto retry;
+            }
+            
+            DateTime timeNow = DateTime.Now;
+            int yearBorn = timeNow.Year - age;
+            Console.WriteLine("You were born in the year " + yearBorn);
+            Console.ReadLine();
+            
+        }
+    }
+}
